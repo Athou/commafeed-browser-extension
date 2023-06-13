a59a0f8beb8c6c5277cc285f372562a117221ecb
Athou <jeremiepanzer@gmail.com>
2023-06-13 15:53:39 +0200

    move refreshBadgeUnreadCount to badge.ts

--- src/app/badge.ts
+++ src/app/badge.ts
@@ -1,4 +1,24 @@
 import browser from "webextension-polyfill"
+import { buildUrl, getOptions } from "./options"
+
+interface UnreadCountEntry {
+    unreadCount: number
+}
+
+export const refreshBadgeUnreadCount = async () => {
+    const options = await getOptions()
+    const url = buildUrl(options.url, "rest/category/unreadCount")
+    const response = await fetch(url, {
+        credentials: "include",
+    })
+
+    let unreadCount: number | undefined
+    if (response.status === 200) {
+        const body: UnreadCountEntry[] = await response.json()
+        unreadCount = body.map(e => e.unreadCount).reduce((a, b) => a + b, 0)
+    }
+    setBadgeUnreadCount(unreadCount)
+}
 
 export const setBadgeUnreadCount = (count: number | undefined) => {
     let label
--- src/entries/background/main.ts
+++ src/entries/background/main.ts
@@ -1,7 +1,6 @@
 import browser from "webextension-polyfill"
-import { setBadgeUnreadCount } from "~/app/badge"
+import { refreshBadgeUnreadCount } from "~/app/badge"
 import { handleEvent } from "~/app/events"
-import { buildUrl, getOptions } from "~/app/options"
 
 // open options when the extension is installed the first time
 browser.runtime.onInstalled.addListener(e => {
@@ -10,34 +9,15 @@ browser.runtime.onInstalled.addListener(e => {
     }
 })
 
-interface UnreadCountEntry {
-    unreadCount: number
-}
-
-const refreshBadge = async () => {
-    const options = await getOptions()
-    const url = buildUrl(options.url, "rest/category/unreadCount")
-    const response = await fetch(url, {
-        credentials: "include",
-    })
-
-    let unreadCount: number | undefined
-    if (response.status === 200) {
-        const body: UnreadCountEntry[] = await response.json()
-        unreadCount = body.map(e => e.unreadCount).reduce((a, b) => a + b, 0)
-    }
-    setBadgeUnreadCount(unreadCount)
-}
-
 // refresh every minute
-browser.alarms.onAlarm.addListener(refreshBadge)
+browser.alarms.onAlarm.addListener(refreshBadgeUnreadCount)
 browser.alarms.create("badge-refresh-timer", { periodInMinutes: 1 })
 
 // refresh at chrome startup
-browser.runtime.onStartup.addListener(refreshBadge)
+browser.runtime.onStartup.addListener(refreshBadgeUnreadCount)
 
 // refresh at firefox startup
-refreshBadge()
+refreshBadgeUnreadCount()
 
 // listen for messages sent from contentScript
 browser.runtime.onMessage.addListener((data: string) => handleEvent(data))
