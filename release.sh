eeb5412c1a6ddba8c7dd85bf9d8f28691cbcc2bc
Athou <jeremiepanzer@gmail.com>
2023-06-13 11:12:14 +0200

    add release script

---
+++ release.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Script to create a new release
+# ------------------------------
+
+# exit on error
+set -e
+
+# make sure we're on master
+BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+if [[ "$BRANCH" != "master" ]]; then
+  echo "You're on branch '$BRANCH', you should be on 'master'."
+  exit
+fi
+
+# make sure CHANGELOG.md has been updated
+read -r -p "Has CHANGELOG.md been updated and committed? (Y/n) " CONFIRM
+case "$CONFIRM" in
+n | N) exit ;;
+esac
+
+read -r -p "New version (x.y.z): " VERSION
+
+npm version $VERSION
+
+read -r -p "Push master and tag $VERSION? (y/N) " CONFIRM
+case "$CONFIRM" in
+y | Y) git push --atomic origin master "v$VERSION" ;;
+esac
